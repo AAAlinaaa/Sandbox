6939706ee0dc9fc63d5d1b4e82822b49a1c743b5
Alina <Alina@hintalo.dark.knight>
2016-12-06 17:40:52 +0100

    Organized imports in Library. Edited class comment.

--- com.b2international.library/src/com/b2international/library/model/Library.java
+++ com.b2international.library/src/com/b2international/library/model/Library.java
@@ -5,14 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Library model class, contains a collection of book objects.
- * 
  * @author Alina
- *
  */
 public class Library implements Serializable {
 	
